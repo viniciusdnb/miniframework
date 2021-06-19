5bf50c08b23fcd1dadccf6254e019e38d78be0a6
vini <viniciusdnb@hotmail.com>
2021-06-18 21:03:51 -0300

    reorganizando diretorios

--- public/index.php
+++ public/index.php
@@ -1,12 +1,8 @@
 <?php
-	include_once __DIR__ . "/vendor/autoload.php";
-
+	include_once __DIR__."/../vendor/autoload.php";
 
 	use src\Route\Controller;
-
 	$d = new Controller;
 	$d->controllClass();
-	
 
-	
 ?>
