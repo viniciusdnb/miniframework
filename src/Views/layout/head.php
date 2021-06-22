a3c19f8cbfaa5e653c495729cc7ee264baf6a543
vini <viniciusdnb@hotmail.com>
2021-06-22 16:44:11 -0300

    correcao dos links para arquivos publico

--- src/Views/layout/head.php
+++ src/Views/layout/head.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="UTF-8">	
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<script src="../public/js/request.js"></script>
+	<script src="js/request.js"></script>
 	<title>Document</title>
 </head>
 <html>
