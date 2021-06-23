b78a91f4439b31ca91eb3d2b39ed7134da6062b3
vini <viniciusdnb@hotmail.com>
2021-06-23 14:42:26 -0300

    correcao dos links para acesso de arquivos js e css publico

--- src/Views/layout/head.php
+++ src/Views/layout/head.php
@@ -3,8 +3,8 @@
 <head>
 	<meta charset="UTF-8">	
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<script src="/public/js/request.js"></script>
-	<link href="/public/css/css.css" rel="stylesheet">
+	<script src="http://<?php echo APP_HOST; ?>js/request.js"></script>
+	<link href="http://<?php echo APP_HOST; ?>css/css.css" rel="stylesheet">
 	<title>Document</title>
 </head>
 <body>
