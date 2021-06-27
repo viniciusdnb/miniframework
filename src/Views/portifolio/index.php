f1590713984d52db91959b278f06071605aacfc8
vini <viniciusdnb@hotmail.com>
2021-06-27 17:57:04 -0300

    ciracao do layout responsivo da pagina portifolio

--- src/Views/portifolio/index.php
+++ src/Views/portifolio/index.php
@@ -1,16 +1,14 @@
 <div class="protifolio">
-<div class="conteudo">
-	<div class="cabecalho">
-		<div  class="cab foto"><p class="pfoto">foto</p></div>
-		<div class="cab perfil"><p class="pperfil">perfil</p>
-			<div class="dados">
-			<div class="perf comum">
-				comum
-			</div>
-			<di class="perf contato">contato</di>
-			</div>
-
-		</div>
-	</div>	
-</div>
+	<div class="flex-container">
+		<div class="flex-item box box-foto">FOTO</div>
+		<div class="flex-item box box-dados">DADOS</div>
+	</div>
+	<div class="flex-container-card">
+		<div class="flex-card">PHP</div>
+		<div class="flex-card">JAVA SCRIPT</div>
+		<div class="flex-card">HTML</div>
+		<div class="flex-card">CSS</div>
+		<div class="flex-card">SQL</div>
+		<div class="flex-card">LARAVEL</div>
+	</div>
 </div>
