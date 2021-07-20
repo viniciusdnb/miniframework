b2160ca789b0aa91905b7ebec9f0db638fa32170
vini <viniciusdnb@hotmail.com>
2021-07-19 21:54:41 -0300

    criando e testando o envio de dados em json atraves do XMLHttpRequest

--- src/Views/login/index.php
+++ src/Views/login/index.php
@@ -1,14 +1,63 @@
 <?php use function src\Libs\route; ?>
-<div>
-	<form action="<?= route("login/auth") ?>" method="POST">
-		<div>
+
+<style>
+	
+</style>
+<div class="container">
+	<div class="box">
+		<div class="form">
+			<form id="login" action="<?= route("login/auth") ?>" method="POST">
+		<div >
 			<label>usuario</label>
-			<input type="text" name="txt_user">
+			<input class="form-control" id="txt_user" type="text" name="txt_user">
 		</div>
-		<div>
+		<div >
 			<label>senha</label>
-			<input type="password" name="txt_pass">
+			<input class="form-control" id="txt_pass" type="password" name="txt_pass">
 		</div>
-		<input type="submit" name="entrar">
+		<div >
+			<input class="btn"type="submit" name="entrar" value="Entrar">
+		</div>
+		
 	</form>
+		</div>
+
+	</div>
+	
 </div>
+<script>
+	const form = document.querySelector("#login");
+	form.addEventListener("submit", login);
+
+	function login(e)
+	{
+		e.preventDefault();
+		let user = form.querySelector("#txt_user").value;
+		let pass = form.querySelector("#txt_pass").value;
+
+		verify(user, pass);
+
+	}
+
+	function verify(user, pass)
+	{
+		if(!user.empty && !pass.empty)
+		{
+
+			const json = JSON.stringify({"user": user, "pass":pass});
+			const xmlhttp = new XMLHttpRequest;
+			xmlhttp.onload = function()
+			{
+				
+				if(xmlhttp.status == 200)
+				{ //ao vir a resposta correta do servidor. vai inserir os dados vindo dentro da tag passada na querySelect;
+					document.querySelector(".container").innerHTML = this.responseText;
+				}
+			}
+			xmlhttp.open(form.method, form.action);
+			xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+			xmlhttp.send("x="+json);
+			
+		}
+	}
+</script>
